079c65606fd5614734732e75968a9896b2a93b38
fasyadita <fasyadita1@gmail.com>
2025-03-14 14:02:08 +0700

    JS-4 Praktikum 1

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,18 +10,19 @@ class UserController extends Controller
 {
     public function index(){
         //tambah user dengan Eloquent Model
-        // $data = [
-        //     'level_id' => 5,
-        //     'username' => 'customer1',
-        //     'nama' => 'Pelanggan',
-        //     'password' => Hash::make('12345')
-        // ];
-        // UserModel::insert($data); // menambahkan data ke tabel m_user
-        
-        $data =[
-            'nama' => 'Pelanggan Pertama'
+        $data = [
+            'level_id' => 2,
+            'username' => 'manager_tiga',
+            'nama' => 'Manager 3',
+            'password' => Hash::make('12345')
         ];
-        UserModel::where('username', 'customer1')->update($data);
+        UserModel::create($data); // menambahkan data ke tabel m_user
+        
+        // $data =[
+        //     'nama' => 'Pelanggan Pertama'
+        // ];
+        // UserModel::where('username', 'customer1')->update($data);
+        
         // coba akses model UserModel
         $user = UserModel::all(); //ambil semua data dari tabel m_user
         return view('user',['data' => $user]);
