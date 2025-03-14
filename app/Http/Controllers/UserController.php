93d46078a31fb89b9aea12fa1106b95b64507eb0
fasyadita <fasyadita1@gmail.com>
2025-03-14 14:35:03 +0700

    JS-4 Praktikum 2.2

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,13 +10,13 @@ class UserController extends Controller
 {
     public function index(){
         //tambah user dengan Eloquent Model
-        $data = [
-            'level_id' => 2,
-            'username' => 'manager_tiga',
-            'nama' => 'Manager 3',
-            'password' => Hash::make('12345')
-        ];
-        UserModel::create($data); // menambahkan data ke tabel m_user
+        // $data = [
+        //     'level_id' => 2,
+        //     'username' => 'manager_tiga',
+        //     'nama' => 'Manager 3',
+        //     'password' => Hash::make('12345')
+        // ];
+        //UserModel::create($data); // menambahkan data ke tabel m_user
         
         // $data =[
         //     'nama' => 'Pelanggan Pertama'
@@ -24,7 +24,26 @@ class UserController extends Controller
         // UserModel::where('username', 'customer1')->update($data);
         
         // coba akses model UserModel
-        $user = UserModel::all(); //ambil semua data dari tabel m_user
-        return view('user',['data' => $user]);
+        // $user = UserModel::all(); //ambil semua data dari tabel m_user
+        // return view('user',['data' => $user]);
+
+        // $user = UserModel::find(1); //melakukan pencarian sdata dari tabel m_user
+        // return view('user',['data' => $user]);
+
+        // $user = UserModel::where('level_id',1)->first(); //melakukan pencarian data dari tabel m_user
+        // return view('user',['data' => $user]);
+
+        // $user = UserModel::firstwhere('level_id',1); //melakukan pencarian data dari tabel m_user
+        // return view('user',['data' => $user]);
+
+        // $user = UserModel::findOr(1, ['username', 'nama'], function(){
+        //     abort(404);
+        // });
+        // return view('user', ['data' => $user]);
+
+        $user = UserModel::findOr(20, ['username', 'nama'], function(){
+            abort(404);
+        });
+        return view('user', ['data' => $user]);
     }
 }
