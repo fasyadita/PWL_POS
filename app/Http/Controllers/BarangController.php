11d13cb085884237c57904c1db94204d8f3e12e9
fasyadita <fasyadita1@gmail.com>
2025-04-27 14:10:02 +0700

    JS 8 - Praktikum 3

--- app/Http/Controllers/BarangController.php
+++ app/Http/Controllers/BarangController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\BarangModel;
 use Illuminate\Http\Request;
 use App\Models\KategoriModel;
+use Barryvdh\DomPDF\Facade\Pdf;
 use Yajra\DataTables\DataTables;
 use PhpOffice\PhpSpreadsheet\IOFactory;
 use Illuminate\Support\Facades\Validator;
@@ -407,5 +408,23 @@ class BarangController extends Controller
         $writer->save('php://output');
         exit;
 
+    }
+
+    public function export_pdf(){
+
+        $barang = BarangModel::select('kategori_id','barang_kode','barang_nama','harga_beli','harga_jual')
+        ->orderBy('kategori_id')
+        ->orderBy('barang_kode')
+        ->with('kategori')
+        ->get();
+
+        // user barryvdh\DomPdf\facade\Pdf
+        $pdf = Pdf::loadView('barang.export_pdf',['barang' => $barang]);
+        $pdf-> setPaper('a4','portrait'); //set ukuran kertas orientasi
+        $pdf-> setOption("IsRemoteEnabled", true); //set true jika ada gambar dr url
+        $pdf->render();
+
+        return $pdf->stream('Data Barang' .date('Y-m-d H:i:s').'.pdf');
+    
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -132,6 +132,7 @@ Route::middleware(['auth'])->group(function(){ // semua route di dalam group ini
             Route::get('/import',[BarangController::class, 'import']); // ajax form
             Route::post('/import_ajax', [BarangController::class, 'import_ajax']); //ajax import excel
             Route::get('/export_excel',[BarangController::class, 'export_excel']); // export excel
+            Route::get('/export_pdf',[BarangController::class, 'export_pdf']); // export pdf
         });
     });
 
